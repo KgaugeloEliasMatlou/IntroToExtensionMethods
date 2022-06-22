a20e574a03ccdc9af220d7858f7c20d2a5f3e55a
Train 08 <Train 08@DESKTOP-0JKGM9D>
2022-06-22 10:50:59 +0200

    Add project files.

---
+++ IntroToExtensionMethods/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string x = "hello";
+        string y = "Hello";
+        Console.WriteLine(x + " is capitalized ?" + x.Iscap());
+        Console.WriteLine(y + " is capitalized ?" + y.Iscap());
+
+
+    }
+
+}
+public static class Stringcap
+{
+    public static bool Iscap(this string s)
+    {
+        if (string.IsNullOrEmpty(s)) return false;
+        return char.IsUpper(s[0]);
+    }
+}
